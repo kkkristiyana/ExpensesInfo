b8c725ed929846dc8f3975fe14068d093f477469
PCK102 <PCK102@K102>
2025-12-12 08:42:57 +0200

    Playground JS added

---
+++ ExpensesInfo/Controllers/PlaygroundController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesInfo.Controllers
+{
+    public class PlaygroundController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
